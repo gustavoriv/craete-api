ec4e4cd1f31204ed33eb232d0905a23f9005d13f
pablomaida <pablomaidana110@gmail.com>
2023-07-08 15:20:20 -0400

    cambio de ping a reservas

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,6 +4,6 @@ export const index = (req, res) =>
   res.json({ message: "Api de prueba para la biblioteca" });
 
 export const ping = async (req, res) => {
-  const [result] = await pool.query("SELECT * from informe");
+  const [result] = await pool.query("SELECT * from reservas");
   res.json(result);
 };
